14ee13e58fb6e9ae0eb8dc9387f2c6f0d2bcb7b9
Ali-Amir Aldan <aliamir@mit.edu>
2016-01-18 23:23:48 -0500

    Added initial version of EncodedMessage class. Updated Scout logic. Enemy location now stores den locations.

--- battlecode-scaffold-master/src/team316/navigation/EnemyLocationModel.java
+++ battlecode-scaffold-master/src/team316/navigation/EnemyLocationModel.java
@@ -1,10 +1,30 @@
 package team316.navigation;
 
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
+
 public class EnemyLocationModel {
 
+	final Set<MapLocation> knownZombieDens;
+	final Queue<Integer> notificationsPending;
+
 	public EnemyLocationModel() {
+		knownZombieDens = new HashSet<>();
+		notificationsPending = new LinkedBlockingQueue<Integer>();
 	}
-	
+
+	public void addZombieDenLocation(RobotInfo r) {
+		if (!knownZombieDens.contains(r.location)) {
+			knownZombieDens.add(r.location);
+			notificationsPending.append(EncodedMessage.zombieDenLocation(r.location));
+		}
+	}
+
 	public void onNewTurn() {
 	}
 }
---
+++ battlecode-scaffold-master/src/team316/utils/EncodedMessage.java
@@ -0,0 +1,28 @@
+package team316.utils;
+
+import battlecode.common.MapLocation;
+
+public class EncodedMessage {
+
+	// Please, append new types to the end.
+	public enum MessageType {
+		EMPTY_MESSAGE, ZOMBIE_DEN_LOCATION
+	}
+
+	/**
+	 * @param loc
+	 *            Location of the zombie den.
+	 * @return Message encoding zombie den location.
+	 */
+	public static int zombieDenLocation(MapLocation loc) {
+		return MessageType.ZOMBIE_DEN_LOCATION.ordinal()
+				+ (encodeLocation20bits(loc) << 4);
+	}
+
+	public static int encodeLocation20bits(MapLocation loc) {
+		int encoding = (loc.x << 10) + loc.y;
+		// To get x back do: (encoding >> 10).
+		// To get y back do: (encoding & 1023).
+		return encoding;
+	}
+}
